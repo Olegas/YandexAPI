020fb471eaf515258f1d7d4e4b9b639af12b2b8b
Olegas <oleg@elifantiev.ru>
2011-04-17 00:10:14 +0400

    Licence added

--- src/ru/elifantiev/yandex/YandexSSLSocketFactory.java
+++ src/ru/elifantiev/yandex/YandexSSLSocketFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Oleg Elifantiev
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package ru.elifantiev.yandex;
 
 import org.apache.http.conn.ssl.SSLSocketFactory;
@@ -16,7 +32,7 @@ class YandexSSLSocketFactory extends SSLSocketFactory {
 
     SSLContext sslContext = SSLContext.getInstance("TLS");
 
-    public YandexSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+    YandexSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
         super(truststore);
 
         TrustManager tm = new X509TrustManager() {
--- src/ru/elifantiev/yandex/oauth/OAuthActivity.java
+++ src/ru/elifantiev/yandex/oauth/OAuthActivity.java
@@ -1,13 +1,33 @@
+/*
+ * Copyright 2011 Oleg Elifantiev
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package ru.elifantiev.yandex.oauth;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 
 
 abstract public class OAuthActivity extends Activity {
 
-    public static final String ACCESS_TOKEN_EXTRA = "ru.elifantiev.oauth.ACCESS_TOKEN";
-    public static final String ACCESS_ERROR_EXTRA = "ru.elifantiev.oauth.ACCESS_ERROR";
+    public static final String EXTRA_AUTH_RESULT = "ru.elifantiev.yandex.oauth.AUTH_RESULT_EXTRA";
+    public static final String EXTRA_AUTH_RESULT_ERROR = "ru.elifantiev.yandex.oauth.AUTH_RESULT_ERROR_EXTRA";
+    public static final String ACTION_AUTH_RESULT = "ru.elifantiev.yandex.oauth.AUTH_RESULT";
+    public static final int AUTH_RESULT_OK = 0;
+    public static final int AUTH_RESULT_ERROR = 1;
 
     @Override
     protected void onResume() {
@@ -29,8 +49,6 @@ abstract public class OAuthActivity extends Activity {
     abstract protected String getClientId();
     abstract protected String getAppId();
     abstract protected Uri getServer();
-    abstract protected void onAuthorizationGranted(AccessToken token);
-    abstract protected void onAuthorizationFailed(String reason);
     abstract protected AccessTokenStorage getTokenStorage();
 
     protected AsyncContinuationHandler getContinuationHandler() {
@@ -40,14 +58,18 @@ abstract public class OAuthActivity extends Activity {
     protected AuthStatusHandler getDefaultStatusHandler() {
         return new AuthStatusHandler() {
             public void onResult(AuthResult result) {
+                Intent callHome = new Intent(ACTION_AUTH_RESULT);
                 if(result.isSuccess()) {
                     getTokenStorage().storeToken(result.getToken(), getAppId());
-                    onAuthorizationGranted(result.getToken());
+                    callHome.putExtra(EXTRA_AUTH_RESULT, AUTH_RESULT_OK);
                 }
-                else
-                    onAuthorizationFailed(result.getError());
+                else  {
+                    callHome
+                        .putExtra(EXTRA_AUTH_RESULT, AUTH_RESULT_ERROR)
+                        .putExtra(EXTRA_AUTH_RESULT_ERROR, result.getError());
+                }
+                startActivity(callHome);
             }
         };
     }
-
 }
